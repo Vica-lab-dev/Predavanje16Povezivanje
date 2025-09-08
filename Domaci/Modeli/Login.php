62597a109859c93f7079ddc7e73c89b5149dec58
Vica-lab-dev <vicomaletkovic@gmail.com>
2025-09-08 09:25:51 +0200

    Ispravka greske POST/GET

--- Domaci/Modeli/Login.php
+++ Domaci/Modeli/Login.php
@@ -2,18 +2,18 @@
 
     require_once "../Modeli/baza.php";
 
-    if(!isset($_POST["LoginEmail"]) || empty($_POST["LoginEmail"]))
+    if(!isset($_GET["LoginEmail"]) || empty($_GET["LoginEmail"]))
     {
         die("Niste uneli Vas email!");
     }
 
-    if(!isset($_POST["LoginSifra"]) || empty($_POST["LoginSifra"]))
+    if(!isset($_GET["LoginSifra"]) || empty($_GET["LoginSifra"]))
     {
         die("Niste uneli Vasu lozinku!");
     }
 
-    $email = $_POST["LoginEmail"];
-    $sifra = $_POST["LoginSifra"];
+    $email = $_GET["LoginEmail"];
+    $sifra = $_GET["LoginSifra"];
 
     $rezultat = $baza->query("SELECT * FROM korisnici WHERE email = '$email'");
 
